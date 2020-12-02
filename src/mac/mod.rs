7a4dc1abaf05e9e8e1cfa51ebecd9dbfc9186122
Nym Seddon <unseddd@shh.xyz>
2020-12-02 00:10:53 +0000

    Set 4: Challenge 29

--- src/mac/mod.rs
+++ src/mac/mod.rs
@@ -1,32 +1,53 @@
 use alloc::vec::Vec;
-use rand::{Rng, thread_rng};
+
+use rand::{thread_rng, Rng};
 
 /// Secret-prefix MAC implementation using SHA-1
 pub struct Sha1SecretMac {
-    // use a rediculously short key for test purposes
-    key: u8,
+    key: u128,
 }
 
 impl Sha1SecretMac {
     /// Create a new secret-prefix MAC producer
     pub fn new() -> Self {
-        Self { key: thread_rng().gen_range::<u8, u8, u8>(0, 255) }
+        Self {
+            key: thread_rng().gen_range::<u128, u128, u128>(1, core::u128::MAX),
+        }
     }
 
     /// Convenience function to create a MAC producer from a given key
-    pub fn from_key(key: u8) -> Self {
+    pub fn from_key(key: u128) -> Self {
         Self { key: key }
     }
 
-   
     /// Calculate a secret-prefix MAC over the given message
     ///
     /// Prepends the message with the secret key, before calculating the SHA-1 digest
     pub fn mac(&self, msg: &[u8]) -> Result<[u8; isha1::DIGEST_LEN], isha1::Error> {
-        let mut input: Vec<u8> = Vec::with_capacity(msg.len() + 1);
-        input.push(self.key);
+        let mut input: Vec<u8> = Vec::with_capacity(msg.len() + core::mem::size_of_val(&self.key));
+
+        input.extend_from_slice(self.key.to_le_bytes().as_ref());
         input.extend_from_slice(&msg);
 
         isha1::Sha1::digest(&input)
+    }
+
+    /// Calculates a secret-prefix MAC over the given message
+    ///
+    /// Initializes the SHA-1 state from the given MAC
+    /// For Cryptopals challenge 29, NEVER do this in practice
+    ///
+    /// Simulates manipulating SHA-1 state with "glue padding" in a real attack
+    pub fn mac_from_mac(
+        &self,
+        msg: &[u8],
+        mac: &[u8; isha1::DIGEST_LEN],
+        total_len: u64,
+    ) -> Result<[u8; isha1::DIGEST_LEN], isha1::Error> {
+        let mut sha = isha1::Sha1::from_digest(mac);
+
+        sha.input(&msg)?;
+
+        sha.finalize_insecure(total_len)
     }
 }
--- tests/set_four/mod.rs
+++ tests/set_four/mod.rs
@@ -1,8 +1,8 @@
 #[test]
 fn challenge_twenty_five() {
+    use crate::common::read_lines;
     use cryptopals::ctr::edit;
     use cryptopals::encoding::from_base64;
-    use crate::common::read_lines;
 
     let editor = edit::EditCtr::new();
     let b64 = read_lines("tests/res/set4_challenge25.txt");
@@ -52,12 +52,12 @@ fn challenge_twenty_seven() {
     let block_one = output.ciphertext[..BLOCK_LEN].to_vec();
 
     // set the second ciphertext block to all zeroes
-    output.ciphertext[BLOCK_LEN..BLOCK_LEN*2].copy_from_slice(&[0; BLOCK_LEN]);
+    output.ciphertext[BLOCK_LEN..BLOCK_LEN * 2].copy_from_slice(&[0; BLOCK_LEN]);
 
     // set the third ciphertext block to the first ciphertext block
-    // this results in the plaintext: decrypt(ciphertexttext[0]) ^ 0..0 
+    // this results in the plaintext: decrypt(ciphertexttext[0]) ^ 0..0
     // the first ciphertext block will get decrypted and XORed with the IV
-    output.ciphertext[BLOCK_LEN*2..BLOCK_LEN*3].copy_from_slice(&block_one);
+    output.ciphertext[BLOCK_LEN * 2..BLOCK_LEN * 3].copy_from_slice(&block_one);
 
     // set the IV to the key, so first plaintext block:
     // decrypt(ciphertext[0]) ^ key
@@ -74,7 +74,11 @@ fn challenge_twenty_seven() {
     // since plaintext[0] = decrypt(ciphertext[0]) ^ key,
     // and plaintext[2] = decrypt(ciphertext[0])
     // plaintext[0] ^ plaintext[2] == key
-    let found_key = craes::xor(&plaintext[..BLOCK_LEN], &plaintext[BLOCK_LEN*2..BLOCK_LEN*3]).unwrap();
+    let found_key = craes::xor(
+        &plaintext[..BLOCK_LEN],
+        &plaintext[BLOCK_LEN * 2..BLOCK_LEN * 3],
+    )
+    .unwrap();
 
     assert_eq!(found_key.as_slice(), output.key.as_ref());
 }
@@ -85,13 +89,15 @@ fn challenge_twenty_eight() {
 
     let mut msg = b"real or random?".to_vec();
 
+    let key = 0x420691337_u128;
+
     for i in 0..=255 {
-        let i_macer = Sha1SecretMac::from_key(i);
+        let i_macer = Sha1SecretMac::from_key(key + i);
         let i_mac = i_macer.mac(msg.as_ref()).unwrap();
 
         for j in 0..=255 {
             if i != j {
-                let j_macer = Sha1SecretMac::from_key(j);
+                let j_macer = Sha1SecretMac::from_key(key + j);
                 let j_mac = j_macer.mac(msg.as_ref()).unwrap();
 
                 // "prove" that we can't find a collision using a different key
@@ -116,4 +122,46 @@ fn challenge_twenty_eight() {
         // "prove" that we can't find a collision using a different message
         assert!(new_mac != mac);
     }
+}
+
+#[test]
+fn challenge_twenty_nine() {
+    use cryptopals::mac::Sha1SecretMac;
+
+    let key = 0x4_2069_1337_u128;
+    let orig_msg = b"comment1=cooking%20MCs;userdata=foo;comment2=%20like%20a%20pound%20of%20bacon";
+
+    // for convenience, create the full message to get the proper length needed for padding
+    let mut full_msg = key.to_le_bytes().to_vec();
+    full_msg.extend_from_slice(orig_msg.as_ref());
+
+    let macer = Sha1SecretMac::from_key(key);
+
+    // simulate getting a secret-prefix MAC of the original message
+    let orig_mac = macer.mac(orig_msg.as_ref()).unwrap();
+
+    // attacker appends forged message, and calculates a new signature without knowledge of the key
+    let forge_msg = b";admin=true";
+
+    // copy the original message and padding, without the secret key
+    // represents info available to the attacker
+    let mut full_forge_msg = orig_msg.to_vec();
+
+    let mut msg_padding = isha1::Sha1::pad_message(&full_msg).unwrap();
+
+    msg_padding.extend_from_slice(forge_msg.as_ref());
+
+    full_forge_msg.extend_from_slice(&msg_padding);
+
+    // forge a MAC with the SHA-1 state fixed at the result of the original MAC
+    let total_forge_len = ((core::mem::size_of_val(&key) + full_forge_msg.len()) * 8) as u64;
+    let forge_mac = macer
+        .mac_from_mac(forge_msg.as_ref(), &orig_mac, total_forge_len)
+        .unwrap();
+
+    // simulate supplying the full forged message to the original MACer
+    let new_mac = macer.mac(&full_forge_msg).unwrap();
+
+    // verify it matches the forged MAC
+    assert_eq!(forge_mac, new_mac);
 }
