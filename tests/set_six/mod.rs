bf746046787fccaa3acbb2c099c8f98a77a0c8f3
Nym Seddon <unseddd@shh.xyz>
2020-12-21 23:37:36 +0000

    Set 6: Challenge 42

--- tests/set_six/mod.rs
+++ tests/set_six/mod.rs
@@ -8,7 +8,7 @@ fn challenge_forty_one() {
     let msg = b"unreadable bytes";
     let sk = irsa::RsaPrivateKey::from_exponent(3, irsa::RSA_1024_LEN).unwrap();
     let pk = irsa::RsaPublicKey::from(&sk);
-    let ciphertext =  pk.encrypt(msg.as_ref()).unwrap();
+    let ciphertext = pk.encrypt(msg.as_ref()).unwrap();
 
     // S = random number > 1 < N
     let s = thread_rng().gen_range::<u128, u128, u128>(2, u128::MAX);
@@ -35,4 +35,38 @@ fn challenge_forty_one() {
     let recovered = p.to_bytes_be();
 
     assert_eq!(recovered[..], msg[..]);
+}
+
+#[test]
+fn challenge_forty_two() {
+    let forge_msg = b"hi mom";
+
+    // encode the message using PKCS#1-v1.5
+    let encoded =
+        irsa::pkcs1v1_5::encode(forge_msg.as_ref(), irsa::RSA_1024_LEN / 8, irsa::Hash::Sha1)
+            .unwrap();
+
+    // ensure the leading forged message bytes follow PKCS#1-v1.5 format
+    let mut forge_encoded = [0x00, 0x01, 0xff, 0x00].to_vec();
+
+    // take the last bytes of the encoded message (ASN.1 || HASH(m))
+    forge_encoded.extend_from_slice(&encoded[encoded.len() - 35..]);
+    for _i in forge_encoded.len()..(irsa::RSA_1024_LEN / 8) {
+        // fill the end with non-zero trash
+        forge_encoded.push(0xff);
+    }
+
+    // cube root the result
+    let forge_bn = BigUint::from_bytes_be(&forge_encoded);
+    let forge_sig = forge_bn.cbrt().to_bytes_be();
+
+    // create random 1024-bit private/public key with public exponent 3
+    let priv_key = irsa::RsaPrivateKey::from_exponent(3, irsa::RSA_1024_LEN).unwrap();
+    let pub_key = irsa::RsaPublicKey::from(&priv_key);
+
+    // check that the forged message passes verification
+    let res = pub_key
+        .verify_pkcs1_v1_5_insecure(forge_msg.as_ref(), &forge_sig, irsa::Hash::Sha1)
+        .unwrap();
+    assert_eq!(res, irsa::Verification::Consistent);
 }
